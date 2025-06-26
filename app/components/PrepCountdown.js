539419981fef0bd090bbf2b38e32d1b1a091e95a
susnfor <97429786+Susnfor@users.noreply.github.com>
2025-06-26 19:02:44 +0100

    Refactor: Remove unused countdown and question loader components

--- app/components/PrepCountdown.js
+++
@@ -1,6 +0,0 @@
-import CountdownComponent from "./CountdownComponent";
-export default function PrepCountdown({ time, onComplete, label = 'Prep Countdown' }) {
-    return (
-        <CountdownComponent time={time} onComplete={onComplete} label={label} />
-    );
-}
--- app/components/RecordingCountdown.js
+++
@@ -1,6 +0,0 @@
-import CountdownComponent from "./CountdownComponent";
-export default function RecordingCountdown({ time, onComplete, label = 'Recording Countdown' }) {
-    return (
-        <CountdownComponent time={time} onComplete={onComplete} label={label} />
-    );
-}
